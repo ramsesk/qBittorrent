6f473283712d87520c1c28360d0d20c0e22a0d63
Ramses Kools <ramses.kools@futurefacts.nl>
2023-11-30 16:34:58 +0100

    duplicate code

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -300,6 +300,20 @@ void nestedLoops() {
     }
 }
 
+void slightlyDifferentNestedLoops() {
+    int big_number = 0;
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            for (int k = 0; k < 10; ++k) {
+                for (int l = 0; l < 10; ++l) {
+                    ++big_number;
+                }
+            }
+        }
+    }
+    big_number += 1000
+}
+
 #if !defined(DISABLE_GUI)
 void showSplashScreen()
 {
